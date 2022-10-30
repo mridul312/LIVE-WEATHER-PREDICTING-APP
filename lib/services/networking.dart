fb01faf826c22efe3af66b99d7e7e9fc02775c3d
mridul312 <80091723+mridul312@users.noreply.github.com>
2022-10-30 09:42:13 +0530

    Update networking.dart

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -7,12 +7,13 @@ class NetworkHelper {
   final String url;
 
   Future getData() async {
-    http.Response response = await http.get(url);
+    http.Response response = await http.get(url); //calling api with the help of http method
+    //this url contains latitude,longitude,api key. we are paasing these parameters to api and in return it will supply json file containing weather
 
     if (response.statusCode == 200) {
       String data = response.body;
 
-      return jsonDecode(data);
+      return jsonDecode(data);//used to convert json data into readable data
     } else {
       print(response.statusCode);
     }
